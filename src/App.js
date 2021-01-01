912e7d0f38b4bdd98a458d7d168d7918361ac781
Kushagra Bhatia <kushagra1198@gmail.com>
2021-01-01 11:18:25 +0530

    routing hats

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Route, Switch} from 'react-router-dom'
+
+const HatsPage = () => (<h1>HATS</h1>)
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/shop/hats' component={HatsPage}/>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 
+import './directory.styes.scss'
+
 class Directory extends React.Component{
     constructor(){
         super()
@@ -45,7 +47,7 @@ class Directory extends React.Component{
     render(){
         return (
             <div className="directory-menu">
-            {this.state.sections.map(({title,imageUrl,id,size}) => (<MenuItem title={title} key={id} imageUrl={imageUrl} size={size}/>))}
+            {this.state.sections.map(({title,imageUrl,id,size,linkUrl}) => (<MenuItem title={title} key={id} imageUrl={imageUrl} size={size} linkUrl={linkUrl}/>))}
             </div>
         )
     }
